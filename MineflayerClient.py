e33b0715e5e272b183e497e496dbbdd158c242a8
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-05-29 17:31:26 +0200

    Fix: yaw

--- MineflayerClient.py
+++ MineflayerClient.py
@@ -199,10 +199,13 @@ class MineflayerClient(MinecraftClient):
 		self._bot.pathfinder.goto(goal)
 		
 	def look_at(self, pitch: float, yaw: float):
-		yaw = radians(yaw) # number of radians to rotate around the vertical axis, starting from the east. Counter clockwise.
+		# number of radians to rotate around the vertical axis, starting from the east; counter clockwise
+		yaw = -(yaw + 180) # idk man, it just works
+		yaw = radians(yaw)
+
 		pitch = radians(-pitch) # in radians; 0 means straight forward. pi / 2 means straight up. -pi / 2 means straight down (the opposite way as MC does)
 		self._bot.look(yaw, pitch, True) # look transition-free
-		sleep(0.5) # give the bot some time to look
+		sleep(1) # give the bot some time to look
 	
 	def hit(self):
 		looking_at = self._bot.blockAtCursor(MineflayerClient.MAX_DISTANCE_MINE_BLOCKS)
